fa5f34751b791e49a95c125f8096cd802abe2cde
Richard Baumann <ohaz@ohaz.engineer>
2022-12-01 20:00:43 +0100

    Add autodownload of input txt:

--- create.py
+++ create.py
@@ -1,5 +1,8 @@
 import os
 import shutil
+from aocd import get_data
+
+YEAR = 2022
 
 if __name__ == '__main__':
     folders = os.listdir('solutions')
@@ -11,3 +14,7 @@ if __name__ == '__main__':
     os.makedirs(target_folder, exist_ok=False)
     shutil.copyfile(source_file, target_file)
     shutil.copyfile(os.path.join('solutions', '__init__.py'), os.path.join(target_folder, '__init__.py'))
+
+    data = get_data(day=newest_day, year=YEAR)
+    with open(os.path.join(target_folder, 'input.txt'), 'w') as f:
+        f.write(data)
